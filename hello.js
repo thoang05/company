6868b678ba35a6a19d44038fbe996657a647838f
Trang Hoang <tychuotsieu45@gmail.com>
2020-04-24 17:18:25 -0400

    test

---
+++ hello.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var port = process.env.PORT || 3000;
+http.createServer(function(req,res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("<h2>Hello World</h2>");
+  res.write("Success! This app is deployed online");
+  res.end();
+}).listen(port);
