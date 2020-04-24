025cc39e98263eb193a22fadb46634f6f6adfdb9
Trang Hoang <tychuotsieu45@gmail.com>
2020-04-24 18:53:11 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -10,6 +10,7 @@ http.createServer(function(req,res) {
   var obj = url.parse(req.url, true).query;
   var name = obj.company;
   var ticker = obj.ticker;
+  
 
   const MongoClient = require('mongodb').MongoClient;
   const uri = "mongodb+srv://thoang05:mongodatabase@cluster0-e3vaj.mongodb.net/test?retryWrites=true&w=majority";
@@ -22,21 +23,22 @@ http.createServer(function(req,res) {
     } 
 
   var database = db.db("Companies");
-  database.collection("companies").findOne({ $or: [{"Company": name}, {"Ticker": ticker}]}, (err, result) => {
+  database.collection("companies").findOne({ $or: [{Company: name}, {Ticker: ticker}]}, (err, result) => {
     /* If result is null (not in database) tell the user that and return */
     if (result == null) {
        res.write("Company Name or Stock Ticker was not found.");
        return;
     }
     /* Otherwise, set the company name to the result and display it to the user */
-     companyname = result.Company;
-     ticker = result.Ticker;
+    var companyname = result.Company;
+    var ticker = result.Ticker;
      res.write("Company Name: " + companyname + "\n" + "Company Ticker: " + ticker);
      /* Close the database */
      db.close();
    })
   });
 
+  res.write("<h2>Hello</h2>");
   res.write("Success! This app is deployed online");
   res.write("Company is:" + name);
   res.end();
