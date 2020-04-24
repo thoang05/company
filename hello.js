1eb483eb197213a1eea34cc77a07679e37b641ba
Trang Hoang <tychuotsieu45@gmail.com>
2020-04-24 18:37:44 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -13,29 +13,29 @@ http.createServer(function(req,res) {
 
   const MongoClient = require('mongodb').MongoClient;
   const uri = "mongodb+srv://thoang05:mongodatabase@cluster0-e3vaj.mongodb.net/test?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// MongoClient.connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true},function(err, db) {
-//  	if (err) {
-//     console.log(err);
-// 		return;
-//   } 
+  MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true},function(err, db) {
+    if (err) {
+      console.log(err);
+      return;
+    } 
 
-//   var database = db.db("Companies");
-//   database.collection("companies").findOne({ $or: [{Company: name}, {Ticker: ticker}]} , (err, result) => {
-//     /* If result is null (not in database) tell the user that and return */
-//     if (result == null) {
-//        res.write("Company Name or Stock Ticker was not found.");
-//        return;
-//     }
-//     /* Otherwise, set the company name to the result and display it to the user */
-//      companyname = result.Company;
-//      ticker = result.Ticker;
-//      res.write("Company Name: " + companyname + "\n" + "Company Ticker: " + ticker);
-//      /* Close the database */
-//      db.close();
-//    })
-//   });
+  var database = db.db("Companies");
+  database.collection("companies").findOne({ $or: [{Company: name}, {Ticker: ticker}]} , (err, result) => {
+    /* If result is null (not in database) tell the user that and return */
+    if (result == null) {
+       res.write("Company Name or Stock Ticker was not found.");
+       return;
+    }
+    /* Otherwise, set the company name to the result and display it to the user */
+     companyname = result.Company;
+     ticker = result.Ticker;
+     res.write("Company Name: " + companyname + "\n" + "Company Ticker: " + ticker);
+     /* Close the database */
+     db.close();
+   })
+  });
 
   res.write("<h2>Hello World</h2>");
   res.write("Success! This app is deployed online");
