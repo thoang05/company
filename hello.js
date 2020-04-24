b8ac1a26e871e3da26586edd05b62f9a506aca48
Trang Hoang <tychuotsieu45@gmail.com>
2020-04-24 19:47:10 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -7,14 +7,12 @@ var port = process.env.PORT || 3000;
 
 http.createServer(function(req,res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  var obj = url.parse(req.url, true).query;
-  var name = obj.company;
-  var ticker = obj.ticker;
+  var obj = url.parse(req.url, true);
+  var info = obj.query.name
   
 
   const MongoClient = require('mongodb').MongoClient;
   const uri = "mongodb+srv://thoang05:mongodatabase@cluster0-e3vaj.mongodb.net/test?retryWrites=true&w=majority";
-  // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
   MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true},function(err, db) {
     if (err) {
@@ -23,7 +21,7 @@ http.createServer(function(req,res) {
     } 
 
   var database = db.db("Companies");
-  database.collection("companies").findOne({ $or: [{Company: name}, {Ticker: ticker}]}, (err, result) => {
+  database.collection("companies").findOne({ $or: [{Company: info}, {Ticker: info}]}, (err, result) => {
     /* If result is null (not in database) tell the user that and return */
     if (result == null) {
        res.write("Company Name or Stock Ticker was not found.");
@@ -31,8 +29,8 @@ http.createServer(function(req,res) {
     }
     /* Otherwise, set the company name to the result and display it to the user */
     var companyname = result.Company;
-    var ticker = result.Ticker;
-     res.write("Company Name: " + companyname + "\n" + "Company Ticker: " + ticker);
+    var companyticker = result.Ticker;
+     res.write("Company Name: " + companyname + "\n" + "Company Ticker: " + companyticker);
      /* Close the database */
      db.close();
    })
@@ -40,6 +38,5 @@ http.createServer(function(req,res) {
 
   res.write("<h2>Hello</h2>");
   res.write("Success! This app is deployed online");
-  res.write("Company is:" + name);
   res.end();
 }).listen(port);
