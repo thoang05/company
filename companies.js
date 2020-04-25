5eed4c86a5580e151335d223b656cffce34e1b1b
Trang Hoang <tychuotsieu45@gmail.com>
2020-04-24 23:00:08 -0400

    Update companies.js

--- companies.js
+++ companies.js
@@ -2,14 +2,17 @@ var http = require('http');
 var url = require('url');
 var MongoClient = require('mongodb').MongoClient;
 
+/* Connecting MongoDB to NodeJS */
 const uri = "mongodb+srv://thoang05:kykL6bVTFofIQhFC@cluster0-e3vaj.mongodb.net/test?retryWrites=true&w=majority";
 
 http.createServer(function(req,res) {
 
   res.writeHead(200, {'Content-Type': 'text/html'});
+  // Take in the query from user's input
   var obj = url.parse(req.url, true).query;
   var info = obj.name
   
+  // Getting into the correct database and collection
   MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true},function(err, db) {
     if (err) {
       console.log(err);
@@ -17,6 +20,8 @@ http.createServer(function(req,res) {
     } 
 
   var database = db.db("Companies");
+
+  // Checking if user's input matches any document in the collection and display results
   database.collection("companies").findOne({ $or: [{Company: info}, {Ticker: info}]}, (err, result) => {
 
     if (result == null) {
@@ -27,10 +32,8 @@ http.createServer(function(req,res) {
     companyname = result.Company;
     companyticker = result.Ticker;
     res.write("<span style='font-size: 2em'>" + "Company Name: " + companyname + "<br>" + "Company Ticker: " + companyticker + "<br><br>" + "Click " + "<a href = https://thoang05.github.io/company/form.html>" + "here" + "</a>" + " to go back to the form" + "</span>");
-
-
      db.close();
-   })
-  });
 
+    })
+  });
 }).listen(process.env.PORT || 3000);
